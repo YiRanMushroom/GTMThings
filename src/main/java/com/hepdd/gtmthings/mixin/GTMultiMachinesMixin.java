ac9709f13e52978962d92389c17d08cabe49ea14
Yiran Wang <yiranmushroom@gmail.com>
2026-02-22 01:30:03 -0800

    Laser and substation energy hatch

--- src/main/java/com/hepdd/gtmthings/mixin/GTMultiMachinesMixin.java
+++ src/main/java/com/hepdd/gtmthings/mixin/GTMultiMachinesMixin.java
@@ -4,6 +4,7 @@ import com.gregtechceu.gtceu.api.pattern.Predicates;
 import com.gregtechceu.gtceu.api.pattern.TraceabilityPredicate;
 import com.gregtechceu.gtceu.common.data.machines.GTAEMachines;
 import com.gregtechceu.gtceu.common.data.machines.GTMultiMachines;
+
 import com.llamalad7.mixinextras.expression.Definition;
 import com.llamalad7.mixinextras.expression.Expression;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
@@ -12,9 +13,10 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(GTMultiMachines.class)
 public class GTMultiMachinesMixin {
+
     @Definition(id = "where", method = "Lcom/gregtechceu/gtceu/api/pattern/FactoryBlockPattern;where(CLcom/gregtechceu/gtceu/api/pattern/TraceabilityPredicate;)Lcom/gregtechceu/gtceu/api/pattern/FactoryBlockPattern;")
     @Expression("?.where('I', @(?))")
-    @ModifyExpressionValue(method = "lambda$static$43", at = @At(value = "MIXINEXTRAS:EXPRESSION"))
+    @ModifyExpressionValue(method = "lambda$static$43", at = @At(value = "MIXINEXTRAS:EXPRESSION"), remap = false)
     private static TraceabilityPredicate addAEBusPattern(TraceabilityPredicate original) {
         return original.or(Predicates.blocks(GTAEMachines.STOCKING_IMPORT_BUS_ME.getBlock(),
                 GTAEMachines.ITEM_IMPORT_BUS_ME.getBlock()));
@@ -22,7 +24,7 @@ public class GTMultiMachinesMixin {
 
     @Definition(id = "where", method = "Lcom/gregtechceu/gtceu/api/pattern/FactoryBlockPattern;where(CLcom/gregtechceu/gtceu/api/pattern/TraceabilityPredicate;)Lcom/gregtechceu/gtceu/api/pattern/FactoryBlockPattern;")
     @Expression("?.where('F', @(?))")
-    @ModifyExpressionValue(method = "lambda$static$43", at = @At(value = "MIXINEXTRAS:EXPRESSION"))
+    @ModifyExpressionValue(method = "lambda$static$43", at = @At(value = "MIXINEXTRAS:EXPRESSION"), remap = false)
     private static TraceabilityPredicate addAEHatchPattern(TraceabilityPredicate original) {
         return original.or(Predicates.blocks(GTAEMachines.STOCKING_IMPORT_HATCH_ME.getBlock(),
                 GTAEMachines.FLUID_IMPORT_HATCH_ME.getBlock()));
---
+++ src/main/java/com/hepdd/gtmthings/mixin/PredicatesMixin.java
@@ -0,0 +1,74 @@
+package com.hepdd.gtmthings.mixin;
+
+import com.gregtechceu.gtceu.api.machine.multiblock.PartAbility;
+import com.gregtechceu.gtceu.api.pattern.Predicates;
+import com.gregtechceu.gtceu.api.pattern.TraceabilityPredicate;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Mixin(Predicates.class)
+public class PredicatesMixin {
+    @Inject(method = "abilities", at = @At("HEAD"), remap = false)
+    private static void gtmthings$abilities(PartAbility[] abilities, CallbackInfoReturnable<TraceabilityPredicate> cir,
+                                            @Local(argsOnly = true) LocalRef<PartAbility[]> abilitiesRef) {
+        // if ability contains INPUT_ENERGY, add INPUT_LASER and SUBSTATION_INPUT_ENERGY if it is not already present
+        // also do that for output energy
+        boolean hasInputEnergy = false;
+        boolean hasOutputEnergy = false;
+        for (PartAbility ability : abilities) {
+            if (ability == PartAbility.INPUT_ENERGY) {
+                hasInputEnergy = true;
+            } else if (ability == PartAbility.OUTPUT_ENERGY) {
+                hasOutputEnergy = true;
+            }
+        }
+
+        List<PartAbility> newAbilities = new ArrayList<>(Arrays.asList(abilities));
+
+        if (hasInputEnergy) {
+            boolean hasInputLaser = false;
+            boolean hasSubstationInputEnergy = false;
+            for (PartAbility ability : abilities) {
+                if (ability == PartAbility.INPUT_LASER) {
+                    hasInputLaser = true;
+                } else if (ability == PartAbility.SUBSTATION_INPUT_ENERGY) {
+                    hasSubstationInputEnergy = true;
+                }
+            }
+            if (!hasInputLaser) {
+                newAbilities.add(PartAbility.INPUT_LASER);
+            }
+            if (!hasSubstationInputEnergy) {
+                newAbilities.add(PartAbility.SUBSTATION_INPUT_ENERGY);
+            }
+        }
+
+        if (hasOutputEnergy) {
+            boolean hasOutputLaser = false;
+            boolean hasSubstationOutputEnergy = false;
+            for (PartAbility ability : abilities) {
+                if (ability == PartAbility.OUTPUT_LASER) {
+                    hasOutputLaser = true;
+                } else if (ability == PartAbility.SUBSTATION_OUTPUT_ENERGY) {
+                    hasSubstationOutputEnergy = true;
+                }
+            }
+            if (!hasOutputLaser) {
+                newAbilities.add(PartAbility.OUTPUT_LASER);
+            }
+            if (!hasSubstationOutputEnergy) {
+                newAbilities.add(PartAbility.SUBSTATION_OUTPUT_ENERGY);
+            }
+        }
+
+        abilitiesRef.set(newAbilities.toArray(new PartAbility[0]));
+    }
+}
