5e225f88af872ce80d509fbb23e02e3c6c430ea0
Yiran Wang <yiranmushroom@gmail.com>
2026-02-22 12:54:37 -0800

    Faster fusion

--- src/main/java/com/hepdd/gtmthings/mixin/FusionReactorMixin.java
+++ src/main/java/com/hepdd/gtmthings/mixin/FusionReactorMixin.java
@@ -1,5 +1,11 @@
 package com.hepdd.gtmthings.mixin;
 
+import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
+import com.gregtechceu.gtceu.api.machine.MetaMachine;
+import com.gregtechceu.gtceu.api.machine.multiblock.WorkableElectricMultiblockMachine;
+import com.gregtechceu.gtceu.api.recipe.GTRecipe;
+import com.gregtechceu.gtceu.api.recipe.OverclockingLogic;
+import com.gregtechceu.gtceu.api.recipe.modifier.ModifierFunction;
 import com.gregtechceu.gtceu.common.machine.multiblock.electric.FusionReactorMachine;
 
 import com.llamalad7.mixinextras.expression.Definition;
@@ -10,13 +16,19 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(FusionReactorMachine.class)
-public class FusionReactorMixin {
+public abstract class FusionReactorMixin extends WorkableElectricMultiblockMachine {
 
-    @Definition(id = "getMaxVoltage", method = "Lcom/gregtechceu/gtceu/api/machine/multiblock/WorkableElectricMultiblockMachine;getMaxVoltage()J")
-    @Definition(id = "min", method = "Ljava/lang/Math;min(JJ)J")
-    @Expression("@(min(?, super.getMaxVoltage()))")
-    @WrapOperation(method = "getMaxVoltage", at = @At("MIXINEXTRAS:EXPRESSION"), remap = false)
-    private long getMaxVoltage(long a, long b, Operation<Long> original) {
-        return b;
+    public FusionReactorMixin(IMachineBlockEntity holder, Object... args) {
+        super(holder, args);
+    }
+
+    @Definition(id = "FUSION_OC", field = "Lcom/gregtechceu/gtceu/common/machine/multiblock/electric/FusionReactorMachine;FUSION_OC:Lcom/gregtechceu/gtceu/api/recipe/OverclockingLogic;")
+    @Definition(id = "getModifier", method = "Lcom/gregtechceu/gtceu/api/recipe/OverclockingLogic;getModifier(Lcom/gregtechceu/gtceu/api/machine/MetaMachine;Lcom/gregtechceu/gtceu/api/recipe/GTRecipe;JZ)Lcom/gregtechceu/gtceu/api/recipe/modifier/ModifierFunction;")
+    @Expression("FUSION_OC.getModifier(?, ?, ?, ?)")
+    @WrapOperation(method = "recipeModifier", at = @At(value = "MIXINEXTRAS:EXPRESSION"), remap = false)
+    private static ModifierFunction fusionUseOverclockingLogic(OverclockingLogic instance, MetaMachine metaMachine, GTRecipe gtRecipe, long voltage, boolean parallel, Operation<ModifierFunction> original) {
+        return original.call(
+                instance, metaMachine, gtRecipe,
+                ((FusionReactorMachine) metaMachine).getOverclockVoltage(), true);
     }
 }
