7b1331bb41dd45f6e3b3a8c125710eb6077e5d9c
Yiran Wang <yiranmushroom@gmail.com>
2026-02-23 22:55:07 -0800

    Cleanroom fix

---
+++ src/main/java/com/hepdd/gtmthings/mixin/CleanroomMachineMixin.java
@@ -0,0 +1,23 @@
+package com.hepdd.gtmthings.mixin;
+
+import com.gregtechceu.gtceu.api.machine.MetaMachine;
+import com.gregtechceu.gtceu.common.machine.multiblock.electric.CleanroomMachine;
+import com.gregtechceu.gtceu.common.machine.multiblock.part.MufflerPartMachine;
+import com.gregtechceu.gtceu.config.ConfigHolder;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(CleanroomMachine.class)
+public class CleanroomMachineMixin {
+
+    @WrapMethod(method = "isMachineBanned", remap = false)
+    private boolean gtmthings$allowCleanroomMachine(MetaMachine machine, Operation<Boolean> original) {
+        if (!ConfigHolder.INSTANCE.machines.cleanMultiblocks && machine instanceof MufflerPartMachine) {
+            return false;
+        } else {
+            return original.call(machine);
+        }
+    }
+}
