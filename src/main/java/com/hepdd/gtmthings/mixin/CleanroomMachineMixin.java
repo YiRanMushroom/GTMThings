ec9307fb40dd615ecb074ca16bfff25b79c9009e
Yiran Wang <yiranmushroom@gmail.com>
2026-02-25 12:45:08 -0800

    still problems with multi furnace.

--- src/main/java/com/hepdd/gtmthings/mixin/CleanroomMachineMixin.java
+++ src/main/java/com/hepdd/gtmthings/mixin/CleanroomMachineMixin.java
@@ -3,7 +3,6 @@ package com.hepdd.gtmthings.mixin;
 import com.gregtechceu.gtceu.api.machine.MetaMachine;
 import com.gregtechceu.gtceu.common.machine.multiblock.electric.CleanroomMachine;
 import com.gregtechceu.gtceu.common.machine.multiblock.part.MufflerPartMachine;
-import com.gregtechceu.gtceu.config.ConfigHolder;
 
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
--- src/main/java/com/hepdd/gtmthings/mixin/GTRecipeModifiersMixin.java
+++ src/main/java/com/hepdd/gtmthings/mixin/GTRecipeModifiersMixin.java
@@ -1,14 +1,15 @@
 package com.hepdd.gtmthings.mixin;
 
 import com.gregtechceu.gtceu.api.machine.MetaMachine;
+import com.gregtechceu.gtceu.api.machine.multiblock.CoilWorkableElectricMultiblockMachine;
 import com.gregtechceu.gtceu.api.recipe.GTRecipe;
+import com.gregtechceu.gtceu.api.recipe.OverclockingLogic;
 import com.gregtechceu.gtceu.api.recipe.modifier.ModifierFunction;
 import com.gregtechceu.gtceu.common.data.GTRecipeModifiers;
 
 import com.llamalad7.mixinextras.expression.Definition;
 import com.llamalad7.mixinextras.expression.Expression;
 import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.ref.LocalIntRef;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,14 +18,27 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(GTRecipeModifiers.class)
 public class GTRecipeModifiersMixin {
 
-    @Inject(method = "multiSmelterParallel", at = @At("MIXINEXTRAS:EXPRESSION"), remap = false)
-    @Definition(id = "builderFunction", method = "Lcom/gregtechceu/gtceu/api/recipe/modifier/ModifierFunction;builder()Lcom/gregtechceu/gtceu/api/recipe/modifier/ModifierFunction$FunctionBuilder;")
-    @Expression("@(builderFunction())")
+    // @Inject(method = "multiSmelterParallel", at = @At("MIXINEXTRAS:EXPRESSION"), remap = false)
+    // @Definition(id = "builderFunction", method =
+    // "Lcom/gregtechceu/gtceu/api/recipe/modifier/ModifierFunction;builder()Lcom/gregtechceu/gtceu/api/recipe/modifier/ModifierFunction$FunctionBuilder;")
+    // @Expression("@(builderFunction())")
+    // private static void multiSmelterParallel(MetaMachine machine, GTRecipe recipe,
+    // CallbackInfoReturnable<ModifierFunction> cir,
+    // @Local(name = "parallels") LocalIntRef parallels,
+    // @Local(name = "baseModifier") ModifierFunction baseModifier,
+    // @Local(name = "ocModifier") ModifierFunction ocModifier) {
+    // parallels.set(parallels.get() / baseModifier.andThen(ocModifier).apply(recipe).parallels);
+    // }
+
+    @Definition(id = "apply", method = "Lcom/gregtechceu/gtceu/api/recipe/modifier/ModifierFunction;apply(Lcom/gregtechceu/gtceu/api/recipe/GTRecipe;)Lcom/gregtechceu/gtceu/api/recipe/GTRecipe;")
+    @Definition(id = "recipe", local = @Local(name = "recipe", type = GTRecipe.class))
+    @Definition(id = "baseModifier", local = @Local(name = "baseModifier", type = ModifierFunction.class))
+    @Expression("baseModifier.apply(recipe)")
+    @Inject(method = "multiSmelterParallel", at = @At("MIXINEXTRAS:EXPRESSION"), remap = false, cancellable = true)
     private static void multiSmelterParallel(MetaMachine machine, GTRecipe recipe,
                                              CallbackInfoReturnable<ModifierFunction> cir,
-                                             @Local(name = "parallels") LocalIntRef parallels,
-                                             @Local(name = "baseModifier") ModifierFunction baseModifier,
-                                             @Local(name = "ocModifier") ModifierFunction ocModifier) {
-        parallels.set(parallels.get() / baseModifier.andThen(ocModifier).apply(recipe).parallels);
+                                             @Local(name = "baseModifier") ModifierFunction baseModifier) {
+        cir.setReturnValue(baseModifier.andThen(OverclockingLogic.PERFECT_OVERCLOCK_SUBTICK
+                .getModifier(machine, recipe, ((CoilWorkableElectricMultiblockMachine) machine).getOverclockVoltage())));
     }
 }
